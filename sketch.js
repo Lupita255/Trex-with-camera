61ea2bf1e5e08ff5f4cf45bb1b8ec72b3e475f73
Lupita255 <75394038+Lupita255@users.noreply.github.com>
2021-03-10 17:58:50 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -48,7 +48,7 @@ function setup() {
   ground.x = ground.width /5;
   ground.velocityX = -(6 + 3 * score/100);
   
-  gameOver = createSprite(canvasX,200,40,10);
+  gameOver = createSprite(displayWidth/2, displayHeight/5);
   gameOver.addImage(gameOverImg);
   
   restart = createSprite(displayWidth/2, 40 + displayHeight/10);
@@ -71,7 +71,7 @@ function setup() {
 
 function draw() {
   //trex.debug = true;
-  background("blue");
+  background("white");
   text("Score: "+ score, 500,50);
   
   if (gameState===PLAY){
